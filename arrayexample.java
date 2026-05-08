20ce532a57d141732b5e23bf4d12913fdcb076df
kripauj <kripauj@gmail.com>
2026-05-08 14:42:44 +0530

    Create arrayexample.java

---
+++ arrayexample.java
@@ -0,0 +1,8 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] arr = {12, 45, 7, 89, 20};
+        for(int i = 0; i < arr.length; i++) {
+        System.out.println("elements are = " + arr[i]);
+    }
+}
+}
