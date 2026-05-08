94882145be69245607c69db2f8a897bf74a66891
kripauj <kripauj@gmail.com>
2026-05-08 14:49:55 +0530

    Update arrayexample.java

--- arrayexample.java
+++ arrayexample.java
@@ -1,8 +1,10 @@
 public class ArrayExample {
     public static void main(String[] args) {
+
         int[] arr = {12, 45, 7, 89, 20};
+
         for(int i = 0; i < arr.length; i++) {
-        System.out.println("elements are = " + arr[i]);
+            System.out.println("Element at index " + i + " = " + arr[i]);
+        }
     }
-}
 }
